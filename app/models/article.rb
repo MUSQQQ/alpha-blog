216989646cfdf7c182e18b328408745c6a403837
Jan Walczak <super.walczak@gmail.com>
2021-02-24 15:33:58 +0100

    test validations for tables content

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
-    
+    validates :title, presence: true, length: {minimum: 6, maximum: 100}    #title is needed to be present in every article + minimum and maximum length of them
+    validates :description, presence: true, length: {minimum: 10, maximum: 300}  #same but with description
 end
