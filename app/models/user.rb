c8c1b495a6f940f3983dab35300bf6577eb0bc4a
Jan Walczak <super.walczak@gmail.com>
2021-03-06 17:27:52 +0100

    Add users valiations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+    #constans are declared by names made out of caps letters only
+    REGEX_FORM = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 5, maximum: 20}
+    validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 50}, format: {with: REGEX_FORM}
 
 end
