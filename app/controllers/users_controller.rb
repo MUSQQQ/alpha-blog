6428b131b404482c2b8571c9d4b1b55abe6f835b
Jan Walczak <super.walczak@gmail.com>
2021-03-07 02:30:26 +0100

    Authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+    before_action :set_user, only: [:show, :edit, :update]
+    
     def new
         @user=User.new
     end
@@ -6,6 +9,7 @@ class UsersController < ApplicationController
     def create
         @user=User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id    #user is automatically logged in after signing up
             flash[:notice] = "Welcome #{@user.username}"
             redirect_to @user
         else
@@ -14,11 +18,11 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user=User.find(params[:id])
+        
     end
 
     def update
-        @user=User.find(params[:id])
+        
         if @user.update(user_params)
             flash[:notice] = "Account updated"
             redirect_to @user
@@ -28,7 +32,7 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user=User.find(params[:id])
+        
         @articles=@user.articles.paginate(page: params[:page], per_page: 5)
     end
 
@@ -40,5 +44,7 @@ class UsersController < ApplicationController
         params.require(:user).permit(:username, :email, :password)
     end
 
-    
+    def set_user
+        @user=User.find(params[:id])
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+    def current_user    #returns detials of the currently logged user
+        if session[:user_id]
+            @current_user ||=User.find(session[:user_id])   #that @current_user ||= lets memorize current user if they are actually logged in. that way we dont block db 
+    
+        end
+    end
+
+    def logged_in?
+        !!current_user      #!! turns something into bool
+    end
+
 end
