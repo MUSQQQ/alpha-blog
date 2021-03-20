79b37e9d41c97702e1f22a1f1786a6c9f7bc05c8
Jan Walczak <super.walczak@gmail.com>
2021-03-20 23:08:28 +0100

    Add many-many assoc articles-categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
     belongs_to :user    #article can only be made by one user
+    has_many :article_categories
+    has_many :categories, through: :article_categories  # many-many implementation
     validates :title, presence: true, length: {minimum: 6, maximum: 100}    #title is needed to be present in every article + minimum and maximum length of them
     validates :description, presence: true, length: {minimum: 10, maximum: 300}  #same but with description
 end
